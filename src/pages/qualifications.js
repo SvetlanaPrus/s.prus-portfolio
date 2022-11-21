62e3fa150a28f81544b91e90f1d865f727c76a36
Svetlana Prus <swetlana.prus@gmail.com>
2022-11-21 23:38:57 +0100

    NEW/Qualif-on: Responsive design

--- src/pages/qualifications.js
+++ src/pages/qualifications.js
@@ -10,62 +10,64 @@ export default function Qualification() {
       <div className="container">
         <p>ABOUT ME</p>
         <h2>Let`s get acquainted</h2>
-        <div className="flex-container">
-          <div className="block">
-            <div className="wrapper-block__img">
-              <img src={IconDeveloper} alt="icon" />
+        <div className="list">
+          <div className="list__flex">
+            <div className="content">
+              <div className="wrapper-block__img">
+                <img src={IconDeveloper} alt="icon" />
+              </div>
+              <h4>Competence:</h4>
+              <ul>
+                <li>HTML, CSS, SCSS, SASS, Figma</li>
+                <li>Javascript, jQuery, React</li>
+                <li>Bootstrap, Material UI</li>
+                <li>Webpack, GitHub, GitLab</li>
+              </ul>
+              <p><strong>Some experience:</strong></p>
+              <p>Typescript, Tailwind CSS, API, Redux, MobX ...</p>
             </div>
-            <h4>Competence:</h4>
-            <ul>
-              <li>HTML, CSS, SCSS, SASS, Figma</li>
-              <li>Javascript, jQuery, React</li>
-              <li>Bootstrap, Material UI</li>
-              <li>Webpack, GitHub, GitLab</li>
-            </ul>
-            <p><strong>Some experience:</strong></p>
-            <p>Typescript, Tailwind CSS, API, Redux, MobX ...</p>
-          </div>
-          <div className="block">
-            <div className="wrapper-block__img">
-              <img src={IconEndorsement} alt="icon" />
+            <div className="content">
+              <div className="wrapper-block__img">
+                <img src={IconEndorsement} alt="icon" />
+              </div>
+              <h4>Certifications:</h4>
+              <ul>
+                <li>Certified Entry-Level JavaScript Programmer</li>
+              </ul>
+              <p><strong>Microsoft Azure Certified:</strong></p>
+              <ul>
+                <li>Fundamentals</li>
+                <li>Data Fundamentals</li>
+                <li>AI Fundamentals</li>
+              </ul>
+              <p><strong>Project Management:</strong></p>
+              <ul>
+                <li>PRINCE2® Foundation</li>
+              </ul>
             </div>
-            <h4>Certifications:</h4>
-            <ul>
-              <li>Certified Entry-Level JavaScript Programmer</li>
-            </ul>
-            <p><strong>Microsoft Azure Certified:</strong></p>
-            <ul>
-              <li>Fundamentals</li>
-              <li>Data Fundamentals</li>
-              <li>AI Fundamentals</li>
-            </ul>
-            <p><strong>Project Management:</strong></p>
-            <ul>
-              <li>PRINCE2® Foundation</li>
-            </ul>
-          </div>
-          <div className="block">
-            <div className="wrapper-block__img">
-              <img src={IconEducation} alt="icon" />
+            <div className="content">
+              <div className="wrapper-block__img">
+                <img src={IconEducation} alt="icon" />
+              </div>
+              <h4>Qualification:</h4>
+              <p>
+                <strong>Bachelor`s degree (5 yrs.)</strong>
+                <br />
+                <strong>Engineer of Information Technologies</strong>
+                <br />
+                * Approved by NOKUT: Bachelor`s degree 180 credits + Master`s degree 60 credits
+              </p>
             </div>
-            <h4>Qualification:</h4>
-            <p>
-              <strong>Bachelor`s degree (5 yrs.)</strong>
-              <br />
-              <strong>Engineer of Information Technologies</strong>
-              <br />
-              * Approved by NOKUT: Bachelor`s degree 180 credits + Master`s degree 60 credits
-            </p>
-          </div>
-          <div className="block">
-            <div className="wrapper-block__img">
-              <img src={IconGirl} alt="icon" />
+            <div className="content">
+              <div className="wrapper-block__img">
+                <img src={IconGirl} alt="icon" />
+              </div>
+              <h4>Personal traits:</h4>
+              <p>
+                I am passionate about frontend development, love coding and not afraid to jump
+                into the unknown. A good team-player, motivated, driven and open-minded.
+              </p>
             </div>
-            <h4>Personal traits:</h4>
-            <p>
-              I am passionate about frontend development, love coding and not afraid to jump
-              into the unknown. A good team-player, motivated, driven and open-minded.
-            </p>
           </div>
         </div>
       </div>
