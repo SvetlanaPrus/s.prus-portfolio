3f923c509dd51099edf475137a0a2949d236f7d7
Svetlana Prus <swetlana.prus@gmail.com>
2022-11-26 22:09:21 +0100

    NEW/Footer: Responsive design

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -22,32 +22,34 @@ export default function Footer() {
   return (
     <section className="footer">
       <div className="container">
-        <div className="flex-container">
-          <div className="foot-left">
-            <animated.a style={effect} href="#top">
-              <h2>SVETLANA PRUS</h2>
-            </animated.a>
-            <a className="phone-block" href="tel:+4797514226">
-              <div className="circle"><img src={Phone} alt="phone" /></div>
-              +47 97 51 42 26
-            </a>
-            <a className="mail-block" href="mailto: swetlana.prus@gmail.com">
-              <div className="circle"><img src={Mail} alt="mail" /></div>
-              swetlana.prus@gmail.com
-            </a>
-          </div>
-          <div className="foot-right">
-            <p>Social media</p>
-            <div className="media-block">
-              <a className="facebook" href="#top">
-                <div className="circle"><img src={Facebook} alt="facebook" /></div>
+        <div className="info">
+          <div className="info-flex">
+            <div className="foot-left">
+              <animated.a style={effect} href="#top">
+                <h2>SVETLANA PRUS</h2>
+              </animated.a>
+              <a className="phone-block" href="tel:+4797514226">
+                <div className="circle"><img src={Phone} alt="phone" /></div>
+                +47 97 51 42 26
               </a>
-              <a className="twitter" href="#top">
-                <div className="circle"><img src={Twitter} alt="twitter" /></div>
+              <a className="mail-block" href="mailto: swetlana.prus@gmail.com">
+                <div className="circle"><img src={Mail} alt="mail" /></div>
+                swetlana.prus@gmail.com
               </a>
-              <a className="instagram" href="#top">
-                <div className="circle"><img src={Instagram} alt="instagram" /></div>
-              </a>
+            </div>
+            <div className="foot-right">
+              <p>Social media</p>
+              <div className="media-block">
+                <a className="facebook" href="#top">
+                  <div className="circle"><img src={Facebook} alt="facebook" /></div>
+                </a>
+                <a className="twitter" href="#top">
+                  <div className="circle"><img src={Twitter} alt="twitter" /></div>
+                </a>
+                <a className="instagram" href="#top">
+                  <div className="circle"><img src={Instagram} alt="instagram" /></div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
