460a65cc0485f7ff1e4f9f8ffdc2a10a310f59c3
Svetlana Prus <swetlana.prus@gmail.com>
2022-11-28 03:15:20 +0100

    NEW/About: Responsive design & corrections

--- src/components/modal-feedback.js
+++ src/components/modal-feedback.js
@@ -1,44 +1,44 @@
 import React from 'react';
+// Pay attention how to use .svg. You need to import, NOT use directly in tag <img/>
+import Cross from '../theme/images/cross.svg';
 
 function ModalFeedback({ author, handleClick }) {
   return (
     <div className="shadow">
       <section className="comment">
-        <div className="container">
-          <div className="icon-block">
-            <a className="icon-cross" href="#top" onClick={handleClick}>
-              <img src="./images/cross.svg" alt="" />
-            </a>
-          </div>
-          <p className="comment-author">
-            {author.name}
-            {' '}
-            {author.surname}
-            {' '}
-            about me:
-          </p>
-          <p className="comment-title">
-            {author.title}
-            ,
-            {' '}
-            {author.company}
-            {' '}
-            -
-            {' '}
-            {author.country}
-            {author.extra
-              ? (
-                <>
-                  <br />
-                  {author.extra}
-                </>
-              )
-              : null}
-          </p>
-          <p className="comment-text">
-            {author.comment}
-          </p>
+        <div className="button-wrapper">
+          <button type="button" onClick={handleClick}>
+            <img src={Cross} alt="cross" />
+          </button>
         </div>
+        <p className="comment-author">
+          {author.name}
+          {' '}
+          {author.surname}
+          {' '}
+          about me:
+        </p>
+        <p className="comment-title">
+          {author.title}
+          ,
+          {' '}
+          {author.company}
+          {' '}
+          -
+          {' '}
+          {author.country}
+          {author.extra
+            ? (
+              <>
+                <br />
+                {author.extra}
+              </>
+            )
+            : null}
+        </p>
+        <p className="comment-text">
+          {author.comment}
+        </p>
       </section>
     </div>
   );
